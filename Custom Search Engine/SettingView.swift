52850b41c555abb34278c0722dace30931ff1140
tsg0o0 <tsg0o0@tsg0o0.com>
2024-03-15 13:27:11 +0900

    Create SettingView.swift (Swift UI)

---
+++ Custom Search Engine/SettingView.swift
@@ -0,0 +1,25 @@
+//
+//  SettingView.swift
+//  Customize Search Engine
+//
+//  Created by Cizzuk on 2024/03/13.
+//
+
+import SwiftUI
+
+@main
+struct MainView: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
