cf8a796a8f423211b71b78f073d6221d2a002798
Cizzuk <cizzuk@tsg0o0.com>
2024-05-21 15:58:32 +0900

    Update background.js

--- Custom Search Engine Extension/Resources/background.js
+++ Custom Search Engine Extension/Resources/background.js
@@ -1,6 +1,6 @@
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.type == "content") {
-        browser.runtime.sendNativeMessage("com.tsg0o0.cse.Extension", {message: "Hello"}, function(response) {
+        browser.runtime.sendNativeMessage("com.tsg0o0.cse.Extension", function(response) {
             const obj = JSON.parse(response);
             if (obj.type == "native") {
                 sendResponse(obj);
