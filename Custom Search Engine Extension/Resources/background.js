37fe18dec72613e7242104bc2bb8cc4e60b232c2
Cizzuk <cizzuk@tsg0o0.com>
2024-05-22 21:23:40 +0900

    Delete unnecessary codes

--- Custom Search Engine Extension/Resources/background.js
+++ Custom Search Engine Extension/Resources/background.js
@@ -2,9 +2,7 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.type == "content") {
         browser.runtime.sendNativeMessage("com.tsg0o0.cse.Extension", function(response) {
             const obj = JSON.parse(response);
-            if (obj.type == "native") {
-                sendResponse(obj);
-            }
+            sendResponse(obj);
         });
     }
     return true;
