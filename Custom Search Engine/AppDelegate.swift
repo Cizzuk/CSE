7f035a9b62275b7e445e86bf0686c06d39f65a1d
Cizzuk <3@cizzuk.net>
2024-09-06 16:01:46 +0900

    Auto Update Settings

--- Custom Search Engine/AppDelegate.swift
+++ Custom Search Engine/AppDelegate.swift
@@ -15,14 +15,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        let currentRegion = Locale.current.regionCode
+        
         let userDefaults = UserDefaults(suiteName: "group.com.tsg0o0.cse")
         let lastVersion = userDefaults!.string(forKey: "LastAppVer") ?? nil
         let urltop = userDefaults!.string(forKey: "urltop") ?? nil
         let urlsuffix = userDefaults!.string(forKey: "urlsuffix") ?? nil
         let searchengine = userDefaults!.string(forKey: "searchengine") ?? nil
         
-        if lastVersion == nil && (urltop != nil || urlsuffix != nil) && searchengine == nil {
-            userDefaults!.set("duckduckgo", forKey: "searchengine")
+        if lastVersion == nil && urltop != nil && urlsuffix != nil && searchengine == nil {
+            userDefaults!.set("google", forKey: "urltop")
+            userDefaults!.set("https://archive.org/search?query=", forKey: "urlsuffix")
+            if currentRegion == "CN" {
+                userDefaults!.set("baidu", forKey: "searchengine")
+            } else {
+                userDefaults!.set("google", forKey: "searchengine")
+            }
+        }
+        
+        if (currentRegion != "CN" && ["baidu", "sogou", "360search"].contains(searchengine))
+           || (currentRegion != "RU" && ["yandex"].contains(searchengine)) {
+            userDefaults!.set("google", forKey: "searchengine")
         }
 
         userDefaults!.set(currentVersion, forKey: "LastAppVer")
