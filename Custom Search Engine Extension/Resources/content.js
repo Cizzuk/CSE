75fef70bfd7642f31f4c67f48939b610206fa643
Cizzuk <3@cizzuk.net>
2025-01-23 23:34:12 +0900

    Update Extension
    #17

--- Custom Search Engine Extension/Resources/content.js
+++ Custom Search Engine Extension/Resources/content.js
@@ -17,7 +17,31 @@ browser.runtime.sendMessage({ url: window.location.href }, function(response) {
         document.getElementsByTagName("html")[0].innerHTML = '<meta name="theme-color" content="#f2f2f7"><body style="background:#f2f2f7"></body>';
     }
     
-    location.replace(cseURL);
+    if (response.postData.length === 0) {
+        location.replace(response.redirectTo);
+    } else {
+        // Remove query
+        const urlNoQuery = window.location.origin + window.location.pathname;
+        window.history.replaceState({}, '', urlNoQuery);
+        
+        // Create <form>
+        const form = document.createElement('form');
+        form.method = 'POST';
+        form.action = response.redirectTo;
+        
+        // Add POST Data
+        response.postData.forEach(item => {
+            const input = document.createElement('input');
+            input.type = 'hidden';
+            input.name = item.key;
+            input.value = item.value;
+            form.appendChild(input);
+        })
+        
+        // Submit form
+        document.body.appendChild(form);
+        form.submit();
+    }
     
-    console.log("CSE: URL has been rewritten.");
+    console.log("CSE: Searched!");
 });
