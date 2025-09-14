fef62004ca4386ead8ca5ba28708843a099fd9a0
Cizzuk <dropcat@cizzuk.net>
2025-09-14 11:08:00 +0900

    update CC code style

--- CSE CCWidget/CCEmojiSearch.swift
+++ CSE CCWidget/CCEmojiSearch.swift
@@ -29,9 +29,7 @@ struct CCEmojiSearch: ControlWidget {
 
 extension CCEmojiSearch {
     struct Provider: ControlValueProvider {
-        var previewValue: Bool {
-            false
-        }
+        var previewValue: Bool { false }
         func currentValue() async throws -> Bool {
             return CSEDataManager.userDefaults.bool(forKey: "useEmojiSearch")
         }
