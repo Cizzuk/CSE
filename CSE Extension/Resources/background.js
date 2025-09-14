72b8afa48bae96d8eaee8169dd88f389a4068948
Cizzuk <dropcat@cizzuk.net>
2025-09-15 00:55:18 +0900

    Update background.js

--- CSE Extension/Resources/background.js
+++ CSE Extension/Resources/background.js
@@ -1,14 +1,12 @@
 'use strict';
 const postRedirectorURL = location.protocol + "//" + location.host + "/post_redirector.html";
 let holdData = {};
-let tabState = {}; // tabId: "wait", "postRedirect", none
 
 // Detect tab updates
 browser.tabs.onUpdated.addListener((tabId, updatedData, tabData) => {
     
     // Ignore if not a valid URL
     if (tabData.url && tabData.status == "loading" && tabData.protocol != "safari-web-extension:") {
-        tabState[tabId] = "wait";
         
         // Send tab data to native app
         browser.runtime.sendNativeMessage("com.tsg0o0.cse.Extension", tabData, function(response) {
@@ -17,14 +15,12 @@ browser.tabs.onUpdated.addListener((tabId, updatedData, tabData) => {
             // type handler
             switch (cseData.type) {
                 case "redirect":
-                    delete tabState[tabId];
                     browser.tabs.update(tabId, {url: cseData.redirectTo});
                     console.log("Redirecting...");
                     break;
                     
                 case "postRedirect":
                     holdData[tabId] = cseData;
-                    tabState[tabId] = "postRedirect";
                     if (cseData.adv_ignorePOSTFallback) {
                         console.log("Waiting post_redirector... (ignore POST Fallback)");
                     } else {
@@ -34,17 +30,14 @@ browser.tabs.onUpdated.addListener((tabId, updatedData, tabData) => {
                     break;
                     
                 case "error":
-                    delete tabState[tabId];
                     console.log("Aborted due to an error.");
                     break;
                     
                 case "cancel":
-                    delete tabState[tabId];
                     console.log("Operation canceled.");
                     break;
 
                 default:
-                    delete tabState[tabId];
                     console.log("Received unknown type.");
                     break;
             }
@@ -56,16 +49,14 @@ browser.tabs.onUpdated.addListener((tabId, updatedData, tabData) => {
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const tabId = sender.tab.id;
     if (request.type = "post_redirector") {
-        if (tabState[tabId] == "postRedirect") {
+        if (holdData[tabId]) {
             console.log("[post_redirector] Redirecting... (with POST).");
             sendResponse(holdData[sender.tab.id]);
-            delete holdData[sender.tab.id];
-            delete tabState[tabId];
-        } else if (tabState[tabId] == "wait") {
+            delete holdData[tabId];
         } else if (sender.url == postRedirectorURL) {
             console.log("[post_redirector] No POST data. Going back...");
             sendResponse({type: "cancel"});
-            browser.tabs.goBack(sender.tab.id);
+            browser.tabs.goBack(tabId);
         } else {
             console.log("[post_redirector] No POST data. Abort.");
             sendResponse({type: "cancel"});
