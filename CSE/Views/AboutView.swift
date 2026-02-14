42fa632585ec2948827ce74dc8fc716c5c473f1b
Cizzuk <dropcat@cizzuk.net>
2026-02-14 22:51:40 +0900

    Update AboutView.swift

--- CSE/Views/AboutView.swift
+++ CSE/Views/AboutView.swift
@@ -35,10 +35,15 @@ struct AboutView: View {
             } header: {
                 Text("Customize Search Engine")
                     .textCase(nil)
+            } footer: {
+                Link("More details on CSE privacy...", destination: URL(string: "https://cizz.uk/cse/privacy-report")!)
+                    .font(.caption)
+                    .padding(.vertical, 4)
             }
             
             Section {} header: {
                 Text("License")
+                    .padding(.top, 20)
             } footer: {
                 Text("MIT License\n\nCopyright (c) 2025 Cizzuk\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the \"Software\"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.")
                     .environment(\.layoutDirection, .leftToRight)
